54edfc515035f91d841709c9093298d1add64405
xyz <xyzzhybz@163.com>
2020-09-14 22:44:41 +0800

    [xyz]finished practice

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,24 @@
-export const getPoetry
+export const getPoetry = () => {
+    return fetch("https://v1.jinrishici.com/all.json",{
+        method: "GET",  
+    }).then(response => {
+        if(response.status == 200){
+            return Promise.resolve(response.json());            
+        }
+        return Promise.reject("error")
+    }).then(data => {
+        return [data.origin,data.author,data.content]
+    }).catch(error => {
+        console.log(error)
+    })
+}
+// export const getPoetry = async () => {
+//     try{
+//         const URL = "https://v1.jinrishici.com/all.json";
+//         const response = await fetch(URL,{method: "GET",});
+//         const data = await response.json();
+//         return [data.origin,data.author,data.content]    
+//     } catch(error){
+//         console.log(error)
+//     }
+// }
